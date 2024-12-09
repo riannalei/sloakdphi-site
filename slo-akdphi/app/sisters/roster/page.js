c6f430419309c546bec5005a6423fe7d8d6ead57
riannalei <riannalei@gmail.com>
2024-12-09 13:13:33 -0800

    recruitment page more styling, moving onto sisters page

---
+++ slo-akdphi/app/sisters/roster/page.js
@@ -0,0 +1,10 @@
+export default function ChapterRosterPage() {
+    return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Chapter Roster</h1>
+        <p>Discover the Chapter Roster of CAL POLY aKDPhi.</p>
+        {/* Add more content about the Chapter Roster */}
+      </div>
+    );
+  }
+  
