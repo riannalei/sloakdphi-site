398695e1dadbad3d47eb5c1fa27c52fa34df3ec1
riannalei <riannalei@gmail.com>
2024-12-18 23:03:16 -0800

    preparing for deployment

--- slo-akdphi/next.config.mjs
+++ slo-akdphi/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Enables static export
+    trailingSlash: true, // Ensures URLs have trailing slashes (important for static hosting)
+    images: {
+      unoptimized: true, // Disables image optimization for static exports
+    },
+  };
+  
+  export default nextConfig;
+  
