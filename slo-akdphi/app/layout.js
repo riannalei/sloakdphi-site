a55b78e641bf5c33adad5c3d2f5e718802753889
riannalei <riannalei@gmail.com>
2024-12-24 18:21:28 -0800

    integrate vercel analytics

--- slo-akdphi/app/layout.js
+++ slo-akdphi/app/layout.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import theme from '../theme';
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
