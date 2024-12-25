5435e9e2d5783ee3362f8a2337f843fdd3cd79fe
riannalei <riannalei@gmail.com>
2024-12-24 18:23:11 -0800

    integrate vercel speed tracking

--- slo-akdphi/app/layout.js
+++ slo-akdphi/app/layout.js
@@ -2,6 +2,7 @@
 
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import theme from '../theme';
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
