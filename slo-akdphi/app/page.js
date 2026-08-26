32211eb75d7822a88add4b16d1901c16964bbfbe
benshree <shreeya.sarav@gmail.com>
2026-08-25 23:37:03 -0700

    updated home page

--- slo-akdphi/app/page.js
+++ slo-akdphi/app/page.js
@@ -152,7 +152,7 @@ export default function HomePage() {
             margin: '0 auto',
           }}
         >
-          Our Winter 2025 Rush is coming soon! Stay tuned for updates and get ready to learn more about alpha Kappa Delta Phi. We can&apos;t wait to meet you!
+          Our Fall 2026 Rush is coming soon! Stay tuned for updates and get ready to learn more about alpha Kappa Delta Phi. We can&apos;t wait to meet you!
         </Typography>
         <Button
           variant="contained"
